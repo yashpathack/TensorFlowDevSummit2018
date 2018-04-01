34fd1f0819cb4c891790b8229748585a7356f432
yashpathack <31958105+yashpathack@users.noreply.github.com>
2018-04-01 12:53:04 +0530

    Update fromkl.py

--- fromkl.py
+++ fromkl.py
@@ -31,3 +31,7 @@ w0 = tf.Variable(initial_value=tf.truncated_normal([3,2],stddev=0.5))
 b0 = tf.Variable(tf.zeroes([2]))
 
 mult = tf.matmul(x,w0) + b0
+
+cross = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels = y_test, logits = multi))
+#Cross entropy or loss function means how poor the function is performing
+#Reduce means the average of the elements of the array
