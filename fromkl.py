0a79df37bb230a92e61bb49431d8ed65d83c27cd
yashpathack <31958105+yashpathack@users.noreply.github.com>
2018-04-01 12:46:03 +0530

    Update fromkl.py

--- fromkl.py
+++ fromkl.py
@@ -26,3 +26,6 @@ x = tf.placeholder(dtype=tf.float32,shape=[None,3])
 y = tf.placeholder(dtype=tf.float32,shape=[None,2])
 
 # Placeholder is just like a promise for the declaration of a datatype
+
+w0 = tf.Variable(initial_value=tf.truncated_normal([3,2],stddev=0.5))
+b0 = tf.Variable(tf.zeroes([2]))
